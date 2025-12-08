243b246973d57944514b3d0ea20a07d9ce3f2e90
Pequu <rzymekrz@gmail.com>
2025-12-08 18:13:36 +0100

    Add files via upload

---
+++ Logowanie/myproject/src/main/java/com/jsfcourse/account/AccountsListBB.java
@@ -0,0 +1,102 @@
+package com.jsfcourse.account;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ejb.EJB;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.Flash;
+import jakarta.servlet.http.HttpSession;
+
+import com.jsfcourse.DAO.AccountsDAO;
+import com.jsfcourse.entities.Accounts;
+
+@Named
+@RequestScoped
+public class AccountsListBB {
+	private static final String PAGE_STAY_AT_THE_SAME = null;
+        private static final String PAGE_ACCOUNTS_EDIT = "accountsEdit?faces-redirect=true";
+
+	private String accName;
+        private String accSurname;
+		
+	@Inject
+	ExternalContext extcontext;
+	
+	@Inject
+	Flash flash;
+	
+	@EJB
+        AccountsDAO accountsDAO;
+		
+	public String getAccSurname() {
+		return accSurname;
+	}
+
+	public void setAccSurname(String accSurname) {
+		this.accSurname = accSurname;
+	}
+        
+        public String getAccName() {
+		return accName;
+	}
+        
+        public void setName(String accName) {
+		this.accName = accName;
+	}
+
+	public List<Accounts> getFullList(){
+		return accountsDAO.getFullList();
+	}
+
+	public List<Accounts> getList(){
+		List<Accounts> list = null;
+		
+		//1. Prepare search params
+		Map<String,Object> searchParams = new HashMap<String, Object>();
+		
+		if (accSurname != null && accSurname.length() > 0){
+			searchParams.put("accSurname", accSurname);
+		}
+		
+		//2. Get list
+		list = accountsDAO.getList(searchParams);
+		
+		return list;
+	}
+        
+        public String newAccounts(){
+		Accounts accounts = new Accounts();
+		
+		//1. Pass object through session
+		//HttpSession session = (HttpSession) extcontext.getSession(true);
+		//session.setAttribute("accounts", accounts);
+		
+		//2. Pass object through flash	
+		flash.put("accounts", accounts);
+		
+		return PAGE_ACCOUNTS_EDIT;
+	}
+
+	public String editAccounts(Accounts accounts){
+		//1. Pass object through session
+		//HttpSession session = (HttpSession) extcontext.getSession(true);
+		//session.setAttribute("accounts", accounts);
+		
+		//2. Pass object through flash 
+		flash.put("accounts", accounts);
+		
+		return PAGE_ACCOUNTS_EDIT;
+	}
+
+	public String deleteAccounts(Accounts accounts){
+		accountsDAO.remove(accounts);
+		return PAGE_STAY_AT_THE_SAME;
+	}
+
+}
---
+++ Logowanie/myproject/src/main/java/com/jsfcourse/entities/Reservations.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jsfcourse.entities;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author rzyme
+ */
+@Entity
+@Table(name = "reservations")
+@NamedQueries({
+    @NamedQuery(name = "Reservations.findAll", query = "SELECT r FROM Reservations r"),
+    @NamedQuery(name = "Reservations.findByIdReservation", query = "SELECT r FROM Reservations r WHERE r.idReservation = :idReservation"),
+    @NamedQuery(name = "Reservations.findByResDate", query = "SELECT r FROM Reservations r WHERE r.resDate = :resDate"),
+    @NamedQuery(name = "Reservations.findByResPayment", query = "SELECT r FROM Reservations r WHERE r.resPayment = :resPayment"),
+    @NamedQuery(name = "Reservations.findByResPrice", query = "SELECT r FROM Reservations r WHERE r.resPrice = :resPrice"),
+    @NamedQuery(name = "Reservations.findByResIsActive", query = "SELECT r FROM Reservations r WHERE r.resIsActive = :resIsActive")})
+public class Reservations implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idReservation")
+    private Integer idReservation;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "resDate")
+    @Temporal(TemporalType.DATE)
+    private Date resDate;
+    @Column(name = "resPayment")
+    private Integer resPayment;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "resPrice")
+    private int resPrice;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "resIsActive")
+    private Boolean resIsActive;
+    @JoinColumn(name = "accounts_idAccount", referencedColumnName = "idAccount")
+    @ManyToOne(optional = false)
+    private Accounts accountsidAccount;
+    @JoinColumn(name = "rooms_idRoom", referencedColumnName = "idRoom")
+    @ManyToOne(optional = false)
+    private Rooms roomsidRoom;
+    @JoinColumn(name = "vouchers_idVoucher", referencedColumnName = "idVoucher")
+    @ManyToOne
+    private Vouchers vouchersidVoucher;
+
+    public Reservations() {
+    }
+
+    public Reservations(Integer idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Reservations(Integer idReservation, Date resDate, int resPrice, Boolean resIsActive) {
+        this.idReservation = idReservation;
+        this.resDate = resDate;
+        this.resPrice = resPrice;
+        this.resIsActive = resIsActive;
+    }
+
+    public Integer getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(Integer idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Date getResDate() {
+        return resDate;
+    }
+
+    public void setResDate(Date resDate) {
+        this.resDate = resDate;
+    }
+
+    public Integer getResPayment() {
+        return resPayment;
+    }
+
+    public void setResPayment(Integer resPayment) {
+        this.resPayment = resPayment;
+    }
+
+    public int getResPrice() {
+        return resPrice;
+    }
+
+    public void setResPrice(int resPrice) {
+        this.resPrice = resPrice;
+    }
+
+    public Boolean getResIsActive() {
+        return resIsActive;
+    }
+
+    public void setResIsActive(Boolean resIsActive) {
+        this.resIsActive = resIsActive;
+    }
+
+    public Accounts getAccountsidAccount() {
+        return accountsidAccount;
+    }
+
+    public void setAccountsidAccount(Accounts accountsidAccount) {
+        this.accountsidAccount = accountsidAccount;
+    }
+
+    public Rooms getRoomsidRoom() {
+        return roomsidRoom;
+    }
+
+    public void setRoomsidRoom(Rooms roomsidRoom) {
+        this.roomsidRoom = roomsidRoom;
+    }
+
+    public Vouchers getVouchersidVoucher() {
+        return vouchersidVoucher;
+    }
+
+    public void setVouchersidVoucher(Vouchers vouchersidVoucher) {
+        this.vouchersidVoucher = vouchersidVoucher;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idReservation != null ? idReservation.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reservations)) {
+            return false;
+        }
+        Reservations other = (Reservations) object;
+        if ((this.idReservation == null && other.idReservation != null) || (this.idReservation != null && !this.idReservation.equals(other.idReservation))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.jsfcourse.entities.Reservations[ idReservation=" + idReservation + " ]";
+    }
+    
+}
